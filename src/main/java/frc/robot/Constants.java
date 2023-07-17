8ed827e2f6443b325eb1e4b1fa1c50f2c2596a16
LiranBratt2121 <brattlirannin@gmail.com>
2023-07-17 13:41:58 +0300

    Constants: Update field values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -8,11 +8,15 @@ import edu.wpi.first.math.util.Units;
 
 public final class Constants {
     public static class VisionConstants {
-        public static final double kCameraHeightMeters = Units.inchesToMeters(28.3464566929);
-        public static final double kTargetHeightMeters = Units.inchesToMeters(23.6220472441);
-        public static final double kCameraPitchRadians = Units.degreesToRadians(0);
-        public static final double kGoalRangeMeters = Units.feetToMeters(3);
+        public static final double kCameraHeightMeters = 0.73;
+        public static final double kTargetHeightMeters = 1.57;
+        public static final double kCameraPitchRadians = Units.degreesToRadians(-1.452);
         public static final String kCameraName = "Microsoft_LifeCam_HD-3000";
+
+        // These values are subjected to change
+        private static final double kFullDistance = 4.42;
+        private static final double kGoal = 2.92;
+        public static final double kGoalRangeMeters = kFullDistance - kGoal;
     }
 
     public static class DriveTrainConstants {
@@ -20,10 +24,11 @@ public final class Constants {
         public static final int kLeftRearPort = 2;
         public static final int kRightFrontPort = 3;
         public static final int kRightRearPort = 4;
+        public static final double kDriveSpeed = 0.11;
     }
 
     public static class PIDConstants {
-        public static final double kp = 0.1;
+        public static final double kp = 1 / 2;
         public static final double ki = 0.0;
         public static final double kd = 0.0;
     }
