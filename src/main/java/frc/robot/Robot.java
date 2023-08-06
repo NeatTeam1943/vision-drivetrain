3ad6bfbc08df27a0de6a249a5c2b0ebe5911c352
LiranBratt2121 <brattlirannin@gmail.com>
2023-08-06 18:53:41 +0300

    Robot: Restet encoders when teleop

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.DriveTrain;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -68,6 +69,8 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousPeriodic() {}
 
+  DriveTrain m_drive = new DriveTrain();
+  
   @Override
   public void teleopInit() {
     // This makes sure that the autonomous stops running when
@@ -77,11 +80,14 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+    m_drive.resetEncoders();
   }
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
